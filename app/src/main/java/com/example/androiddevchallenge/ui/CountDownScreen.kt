4f3e57125906f9163b76b2aaddc7335996972b69
HIDAKA Takahiro <takahr@gmail.com>
2021-03-10 03:15:22 +0900

    add CountDownView

--- app/src/main/java/com/example/androiddevchallenge/ui/CountDownScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/CountDownScreen.kt
@@ -2,7 +2,6 @@ package com.example.androiddevchallenge.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.Easing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -19,23 +18,57 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.theme.CountDownTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.ExperimentalTime
+import kotlin.time.milliseconds
 
-private enum class CountState { Clear, Ready }
+private enum class CountState { Clear, Ready, Count3, Count2, Count1, Launch }
 
+@ExperimentalAnimationApi
+@Composable
+fun CountView(count: Int, visible: Boolean, modifier: Modifier = Modifier) {
+    AnimatedVisibility(visible = visible, modifier = modifier.fillMaxSize()) {
+        Box {
+            Text(text = count.toString(), modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
+
+@ExperimentalTime
 @ExperimentalAnimationApi
 @Composable
 fun CountDownScreen() {
     var countState by remember { mutableStateOf(CountState.Clear) }
+    val coroutineScope = rememberCoroutineScope()
+
+    val countDown: ()->Unit = {
+        coroutineScope.launch {
+            countState = CountState.Count3
+            delay(1000.milliseconds)
+            countState = CountState.Count2
+            delay(1000.milliseconds)
+            countState = CountState.Count1
+            delay(1000.milliseconds)
+            countState = CountState.Launch
+            delay(1000.milliseconds)
+            countState = CountState.Clear
+            delay(100.milliseconds)
+            countState = CountState.Ready
+        }
+    }
 
     LaunchedEffect("CountDownScreen") {
         countState = CountState.Ready
     }
     Box(Modifier.fillMaxSize(1f)) {
+        // start button for Ready state.
         AnimatedVisibility(
             visible = countState == CountState.Ready,
             enter = slideInVertically(
@@ -49,7 +82,7 @@ fun CountDownScreen() {
             modifier = Modifier.fillMaxSize()
         ) {
             Button(
-                onClick = { countState = CountState.Clear },
+                onClick = countDown,
                 modifier = Modifier
                     .align(Alignment.Center)
                     .wrapContentSize()
@@ -57,9 +90,14 @@ fun CountDownScreen() {
                 Text(text = "start")
             }
         }
+        // countdown views
+        CountView(count = 3, visible = countState == CountState.Count3, modifier = Modifier.fillMaxSize())
+        CountView(count = 2, visible = countState == CountState.Count2, modifier = Modifier.fillMaxSize())
+        CountView(count = 1, visible = countState == CountState.Count1, modifier = Modifier.fillMaxSize())
     }
 }
 
+@ExperimentalTime
 @ExperimentalAnimationApi
 @Preview
 @Composable
